5f10c7dc79e513b1e0cfd94d5166a3f9bcb24b13
Alan Gardner <urfolomeus@gmail.com>
2024-11-19 13:35:43 +0000

    Adding which-key to help remember key mappings

---
+++ lua/alan/plugins/which-key.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 500
+  end,
+  opts = {
+    -- your configuration comes here or leave it empty to use
+    -- the default settings, see repo for available options
+  }
+}
+
